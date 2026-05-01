c5c7e31cf622a395e3eee59fd8ff0f167adaa605
Vaseekaran <vaseekaran424@gmail.com>
2026-05-01 21:10:19 +0530

    Add smart task scheduling system functionality
    Implemented a smart task scheduling system that allows users to add tasks, view pending tasks, and execute the highest priority task.

---
+++ Test_case.cpp
@@ -0,0 +1,43 @@
+======================================
+ SMART TASK SCHEDULING SYSTEM (C++)
+======================================
+1. Add New Task
+2. Execute Next (Highest Priority)
+3. View All Pending Tasks
+4. Exit
+Enter choice: 1
+Enter Task ID: 101
+Enter Task Name: VLSI Chip Simulation
+Enter Priority (1-100): 95
+
+[Success] Task added and synchronized with storage.
+
+Enter choice: 1
+Enter Task ID: 102
+Enter Task Name: Documentation Update
+Enter Priority (1-100): 20
+
+[Success] Task added and synchronized with storage.
+
+Enter choice: 1
+Enter Task ID: 103
+Enter Task Name: Urgent Kernel Patch
+Enter Priority (1-100): 100
+
+[Success] Task added and synchronized with storage.
+
+Enter choice: 3
+
+---------------------------------------------
+|    ID |            Task Name |   Priority |
+---------------------------------------------
+|   103 |  Urgent Kernel Patch |        100 |
+|   102 | Documentation Update |         20 |
+|   101 | VLSI Chip Simulation |         95 |
+---------------------------------------------
+
+Enter choice: 2
+
+[Executing] Task: Urgent Kernel Patch (ID: 103)
+
+
